bfa12b087643f7fbc97d2a5e347fcf356723693b
bhimani-akash <bhimani.a@northeastern.edu>
2022-09-07 22:36:38 -0400

    refactored the code

---
+++ grapghql/resolvers/events.js
@@ -0,0 +1,42 @@
+const Event = require("../../models/event");
+
+const { transformEvent } = require("./merge");
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map((event) => {
+        return transformEvent(event);
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  createEvent: async (args) => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: dateToString(args.eventInput.date),
+      creator: "630e97b1af6d4dcf54ddde53",
+    });
+    let createdEvent;
+
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById("630e97b1af6d4dcf54ddde53");
+
+      if (!creator) {
+        throw new Error("User not found.");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
