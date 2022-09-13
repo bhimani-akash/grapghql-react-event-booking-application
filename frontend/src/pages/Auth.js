09a52461bd91e155e84d1cff031aea0f65222417
bhimani-akash <bhimani.a@northeastern.edu>
2022-09-13 00:32:29 -0400

    added login and signup with api integration

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,13 +1,87 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 
-// class AuthPage extends Component {
-//   render() {
-//     return <h1>Auth Page</h1>;
-//   }
-// }
-
-// export default AuthPage;
+import "./Auth.css";
 
 export default function AuthPage() {
-  return <h1>Auth Page</h1>;
+  const emailEl = useRef();
+  const passwordEl = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+
+  const switchModeHandler = (async) => {
+    setIsLogin(!isLogin);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const email = emailEl.current.value;
+    const password = passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password:"${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `,
+    };
+
+    if (isLogin) {
+      requestBody = {
+        query: `
+        mutation {
+          createUser(userInput: {email: "${email}", password: "${password}"}) {
+            _id
+            email
+          }
+        }
+        `,
+      };
+    }
+
+    fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" id="email" ref={emailEl} />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={passwordEl} />
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+        <button type="button" onClick={switchModeHandler}>
+          Switch to {isLogin ? "Signup" : "Login"}
+        </button>
+      </div>
+    </form>
+  );
 }
